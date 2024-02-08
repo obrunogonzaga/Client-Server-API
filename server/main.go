b7211a610be6f5cb142ba4eab4d0bb0bd98c169c
Bruno Gonzaga <brunog86@gmail.com>
2024-02-08 00:09:30 -0300

    feat: implementando banco de dados SQLite

--- server/main.go
+++ server/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 )
 
-type CurrencyQuote struct {
+type CurrencyQuoteDTO struct {
 	USDBRL struct {
 		Ask        string `json:"ask"`
 		Bid        string `json:"bid"`
@@ -21,9 +25,26 @@ type CurrencyQuote struct {
 	} `json:"USDBRL"`
 }
 
+type CurrencyQuote struct {
+	gorm.Model
+	ID        string `gorm:"primaryKey"`
+	Value     float64
+	Code      string
+	Codein    string
+	High      float64
+	Low       float64
+	Name      string
+	PctChange float64
+}
+
 func main() {
+
 	http.HandleFunc("/get-dollar-quote", GetDollarQuoteHandler)
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+
 }
 
 func GetDollarQuoteHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,9 +65,16 @@ func GetDollarQuoteHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+
+	err = createCurrencyDB(dolarQuote)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 }
 
-func GetDollarQuote(cep string) (*CurrencyQuote, error) {
+func GetDollarQuote(cep string) (*CurrencyQuoteDTO, error) {
 	res, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
 	if err != nil {
 		return nil, err
@@ -56,7 +84,63 @@ func GetDollarQuote(cep string) (*CurrencyQuote, error) {
 	if err != nil {
 		return nil, err
 	}
-	var currencyQuote CurrencyQuote
+	var currencyQuote CurrencyQuoteDTO
 	err = json.Unmarshal(body, &currencyQuote)
 	return &currencyQuote, err
+}
+
+func createCurrencyDB(dto *CurrencyQuoteDTO) error {
+
+	if _, err := os.Stat("./data"); os.IsNotExist(err) {
+		err := os.Mkdir("./data", os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+
+	db, err := gorm.Open(sqlite.Open("./data/currency.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	err = db.AutoMigrate(&CurrencyQuote{})
+	if err != nil {
+		return err
+	}
+
+	v := dto.USDBRL
+	fBid, err := strconv.ParseFloat(v.Bid, 64)
+	if err != nil {
+		return err
+	}
+
+	fHigh, err := strconv.ParseFloat(v.High, 64)
+	if err != nil {
+		return err
+	}
+
+	fLow, err := strconv.ParseFloat(v.Low, 64)
+	if err != nil {
+		return err
+	}
+
+	fPctChange, err := strconv.ParseFloat(v.PctChange, 64)
+	if err != nil {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	db.Create(&CurrencyQuote{
+		Value:     fBid,
+		Code:      v.Code,
+		Codein:    v.CodeIn,
+		High:      fHigh,
+		Low:       fLow,
+		Name:      v.Name,
+		PctChange: fPctChange,
+	})
+
+	return nil
+
 }
